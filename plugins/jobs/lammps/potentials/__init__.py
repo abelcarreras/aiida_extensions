a47867bdbe8d12fa5f215314f1bec82c6b9516cb
abel <abelcarreras83@gmail.com>
2017-05-24 14:39:40 +0900

    testing better importing system

--- plugins/jobs/lammps/potentials/__init__.py
+++ plugins/jobs/lammps/potentials/__init__.py
@@ -1,3 +1,4 @@
+import importlib
 
 
 class LammpsPotential:
@@ -9,15 +10,22 @@ class LammpsPotential:
         self._data = data.dict.data
         self._potential_filename = potential_filename
 
-        if self._type == 'tersoff':
-            from tersoff import _generate_LAMMPS_potential, _get_input_potential_lines
-        elif self._type == 'lennard_jones':
-            from lennard_jones import _generate_LAMMPS_potential, _get_input_potential_lines
-        else:
-            raise ValueError('This lammps potential is not implemented')
+        try:
+            potential_module=importlib.import_module(self._type)
+            self._generate_LAMMPS_potential = potential_module._generate_LAMMPS_potential
+            self._get_input_potential_lines = potential_module._get_input_potential_lines
+        except ImportError:
+            raise ImportError('This lammps potential is not implemented')
 
-        self._generate_LAMMPS_potential = _generate_LAMMPS_potential
-        self._get_input_potential_lines = _get_input_potential_lines
+#        if self._type == 'tersoff':
+#            from tersoff import _generate_LAMMPS_potential, _get_input_potential_lines
+#        elif self._type == 'lennard_jones':
+#            from lennard_jones import _generate_LAMMPS_potential, _get_input_potential_lines
+#        else:
+#            raise ValueError('This lammps potential is not implemented')
+
+#        self._generate_LAMMPS_potential = _generate_LAMMPS_potential
+#        self._get_input_potential_lines = _get_input_potential_lines
 
     def get_potential_file(self):
         return self._generate_LAMMPS_potential(self._data)
