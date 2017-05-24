50cdc01b216d675302f3a5b19306326472cc91ef
abel <abelcarreras83@gmail.com>
2017-05-24 18:18:30 +0900

    optimize to True

--- workflows/launcher/launch_phonon_vasp_mp.py
+++ workflows/launcher/launch_phonon_vasp_mp.py
@@ -81,7 +81,7 @@ wf_parameters = {
 
 #Submit workflow
 from aiida.workflows.wf_phonon import WorkflowPhonon
-wf = WorkflowPhonon(params=wf_parameters, optimize=False)
+wf = WorkflowPhonon(params=wf_parameters, optimize=True)
 
 wf.label = 'VASP {}'.format(structure.get_formula())
 wf.start()
