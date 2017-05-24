86c5fdb8a6211de8c8f0dd9a011883f4cf91b80f
abel <abelcarreras83@gmail.com>
2017-05-24 18:27:14 +0900

    optimize to True

--- workflows/launcher/launch_phonon_vasp_mp.py
+++ workflows/launcher/launch_phonon_vasp_mp.py
@@ -10,7 +10,7 @@ import os
 import pymatgen
 
 ##########################
-structure_id = 'mp-804'
+structure_id = 'mp-406'
 ##########################
 
 rester =  pymatgen.MPRester(os.environ['PMG_MAPI_KEY'])
