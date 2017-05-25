141c216278c8f84f55063e9fd97a5973c8b20e3c
abel <abelcarreras83@gmail.com>
2017-05-25 14:41:12 +0900

    Add gruneisen lauch example

---
+++ workflows/launcher/launch_gruneisen_lammps_lj.py
@@ -0,0 +1,86 @@
+
+StructureData = DataFactory('structure')
+ParameterData = DataFactory('parameter')
+
+import numpy as np
+
+
+cell = [[ 3.987594, 0.000000, 0.000000],
+        [-1.993797, 3.453358, 0.000000],
+        [ 0.000000, 0.000000, 6.538394]]
+
+symbols=['Ar'] * 2
+scaled_positions = [(0.33333,  0.66666,  0.25000),
+                    (0.66667,  0.33333,  0.75000)]
+
+structure = StructureData(cell=cell)
+positions = np.dot(scaled_positions, cell)
+
+for i, scaled_position in enumerate(scaled_positions):
+    structure.append_atom(position=np.dot(scaled_position, cell).tolist(),
+                          symbols=symbols[i])
+
+structure.store()
+
+# Example LJ parameters for Argon. These may not be accurate at all
+potential ={'pair_style': 'lennard_jones',
+            #                 epsilon,  sigma, cutoff
+            'data': {'1  1':  '0.01029    3.4    5.0',
+                     #'2  2':   '1.0      1.0    2.5',
+                     #'1  2':   '1.0      1.0    2.5'
+                     }}
+
+
+lammps_machine = {
+    'num_machines': 1,
+    'parallel_env': 'mpi*',
+    'tot_num_mpiprocs': 16}
+
+
+parameters_opt = {'relaxation': 'tri',  # iso/aniso/tri
+                  'pressure': 0.0,  # bars
+                  'vmax': 0.000001,  # Angstrom^3
+                  'energy_tolerance': 1.0e-25,  # eV
+                  'force_tolerance': 1.0e-25,  # eV angstrom
+                  'max_evaluations': 1000000,
+                  'max_iterations': 500000}
+
+# Cluster information
+machine_dict = {
+    'num_machines': 1,
+    'parallel_env':'mpi*',
+    'tot_num_mpiprocs' : 16}
+
+
+# Phonopy input parameters
+ph_dict = ParameterData(dict={'supercell': [[3,0,0],
+                                            [0,3,0],
+                                            [0,0,3]],
+                              'primitive': [[1.0, 0.0, 0.0],
+                                            [0.0, 1.0, 0.0],
+                                            [0.0, 0.0, 1.0]],
+                              'distance': 0.01,
+                              'mesh' : [50, 50, 50]}
+                       ).store()
+
+
+# Collect workflow input data
+wf_parameters = {
+     'structure': structure,
+     'phonopy_input': ph_dict,
+     'input_force': {'code': 'lammps_force@boston',
+                      'potential': potential,
+                      'resources': lammps_machine},
+     'input_optimize': {'code': 'lammps_optimize@boston',
+                         'potential': potential,
+                         'parameters': parameters_opt,
+                         'resources': lammps_machine},
+    }
+
+#Submit workflow
+from aiida.workflows.wf_gruneisen import WorkflowGruneisen
+wf = WorkflowGruneisen(params=wf_parameters)
+
+wf.label = 'Gruneisen LJ Ar'
+wf.start()
+print ('pk: {}'.format(wf.pk))
