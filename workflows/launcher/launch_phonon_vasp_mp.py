65d67403e8286e4d406f691a0bde3bc933f8a71b
abel <abelcarreras83@gmail.com>
2017-06-23 17:13:52 +0900

    add qha plot tool (function version)

--- workflows/launcher/launch_phonon_vasp_mp.py
+++ workflows/launcher/launch_phonon_vasp_mp.py
@@ -46,7 +46,7 @@ def get_supercell_size(structure, max_atoms=100):
             supercell_size = supercell_size_test
 
         cell = np.dot(cell.T, np.diag(supercell_size)).T
-
+        print cell
 
 
 def get_potential_labels(functional, symbol_list, ftype=None):
