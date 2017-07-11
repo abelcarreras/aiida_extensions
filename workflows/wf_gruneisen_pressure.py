17adcdcf1b53647f6b6de916872fe239c55b82ba
abel <abelcarreras83@gmail.com>
2017-07-11 16:08:10 +0900

    Updating tool gruneisen

--- workflows/wf_gruneisen_pressure.py
+++ workflows/wf_gruneisen_pressure.py
@@ -18,7 +18,7 @@ def thermal_expansion(volumes, electronic_energies, gruneisen, stresses=None, t_
 
     tp = gruneisen.get_thermal_properties()
 
-    fit_vs = np.polyfit(volumes, stresses, 2)
+    fit_vs = np.polyfit(stresses, volumes, 2)
 
     free_energy_array = []
     entropy_array = []
