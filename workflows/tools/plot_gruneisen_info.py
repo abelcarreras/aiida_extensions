79e325d3b2924a383b17a0a3a0d082aeb4666ffb
abel <abelcarreras83@gmail.com>
2017-07-11 18:38:04 +0900

    Added stress stensor parsing to lammps

--- workflows/tools/plot_gruneisen_info.py
+++ workflows/tools/plot_gruneisen_info.py
@@ -50,7 +50,7 @@ def get_plot(band_data, q_path, title='', ylabel='', labels=None, q_points=None,
 
 
 #######################
-wf = load_workflow(170)
+wf = load_workflow(168)
 #######################
 
 # Band structure
