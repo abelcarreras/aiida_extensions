f4e8aa875e5b500ad7ebd2e0c783a2142fd1eca0
abel <abelcarreras83@gmail.com>
2017-07-12 16:01:43 +0900

    Testing gruneisen

--- workflows/tools/plot_gruneisen_info.py
+++ workflows/tools/plot_gruneisen_info.py
@@ -50,7 +50,7 @@ def get_plot(band_data, q_path, title='', ylabel='', labels=None, q_points=None,
 
 
 #######################
-wf = load_workflow(214)
+wf = load_workflow(222)
 #######################
 
 # Band structure
