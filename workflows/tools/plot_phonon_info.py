482b88b522ff1d810d0372210f885320d9865999
abel <abelcarreras83@gmail.com>
2017-08-09 10:50:28 +0900

    adding support for Quantum Expresso

--- workflows/tools/plot_phonon_info.py
+++ workflows/tools/plot_phonon_info.py
@@ -15,7 +15,7 @@ import numpy as np
 
 # Set WorkflowPhonon PK number
 #######################
-wf = load_workflow(1149) # load_workflow(437)
+wf = load_workflow(9904) # load_workflow(437)
 #######################
 
 # Phonon Band structure
