e873e2cfc2c135b05dcd6b2bdea4d7f70898f430
abel <abelcarreras83@gmail.com>
2017-08-28 10:49:39 +0900

    Fixed QHA result

--- workflows/wf_qha.py
+++ workflows/wf_qha.py
@@ -588,10 +588,14 @@ class WorkflowQHA(Workflow):
             if abs(max_stress - test_range[1]) < interval and abs(test_range[0] - min_stress) < interval:
                 interval *= 0.5
 
+
             ## Safely measure if the test pressures becomes too close (prevents inactive loop, can be ommited)
-            if min_stress > test_range[0] and max_stress < test_range[1] and total_range / interval < 3:
+            # if min_stress > test_range[0] and max_stress < test_range[1] and total_range / interval < 3:
+            #    interval *= 0.5
+
+            if abs(max - min)/interval < 1:
                 interval *= 0.5
-            ##
+
 
             if max_stress > test_range[1]:
                 self.append_to_report('Increase max {} + {}'.format(test_range[1],
@@ -613,31 +617,6 @@ class WorkflowQHA(Workflow):
 
 
             #  ----------------------------------------------------------------------------
- #           if abs(max_stress - test_range[1]) < interval*1.5 and abs(test_range[0] - min_stress) < interval*1.5:
- #               interval *= 0.5
-
-#            if max_stress < test_range[1]:
-#                self.append_to_report('Decrease max {} - {}'.format(test_range[1],
-#                                                                    np.ceil(np.min([total_range/2, abs(max_stress - test_range[1])]) / interval) * interval))
-#                test_range[1] -= np.ceil(np.min([total_range/2, abs(max_stress - test_range[1])]) / interval) * interval
-
- #           if min_stress > test_range[0]:
- #               self.append_to_report('Decrease min {} + {}'.format(test_range[0],
- #                                                                   np.ceil(np.min([total_range/2, abs(test_range[0] - min_stress)]) / interval) * interval))
- #               test_range[0] += np.ceil(np.min([total_range/2, abs(test_range[0] - min_stress)]) / interval) * interval
-
-
-            # if max > test_range[1] > max_stress + total_range * 1.5:
-            #    max = test_range[1]
-
-            # if min < test_range[0] < min_stress - total_range * 1.5:
-            #    min = test_range[0]
-
-            # if max < test_range[1] < max_stress + total_range * 1.5:
-            #    max = test_range[1]
-
-            # if min > test_range[0] > min_stress - total_range * 1.5:
-            #    min = test_range[0]
 
         total_range = abs(test_range[1] - test_range[0])
         #total_range = abs(max - min)
