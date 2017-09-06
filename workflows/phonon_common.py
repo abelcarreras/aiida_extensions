91c7e7e4d32da7690eadb3f9a8f7fe1a27e968e7
abel <abelcarreras83@gmail.com>
2017-09-06 16:09:10 +0900

    Added structure info function

--- workflows/phonon_common.py
+++ workflows/phonon_common.py
@@ -155,20 +155,20 @@ def get_data_info(structure):
 
     info_data = ''
     info_data += 'Formula: {}\n'.format(formula)
-    info_data += 'Space group: {}   #{}\n'.format(*space_group)
-    info_data += 'Space group: {}   #{}\n'.format(*space_group)
+    info_data += '<br>Space group: {}   #{}\n'.format(*space_group)
+    info_data += '<br>Space group: {}   #{}\n'.format(*space_group)
 
     info_data += '\n'
-    info_data += '<br>Lattice vectors (Angstroms)</br>\n'
-    info_data += ('{0:10.8f} {1:10.8f} {2:10.8f}\n'.format(*lattice_vectors[0]) +
-                  '{0:10.8f} {1:10.8f} {2:10.8f}\n'.format(*lattice_vectors[1]) +
-                  '{0:10.8f} {1:10.8f} {2:10.8f}\n'.format(*lattice_vectors[2]))
+    info_data += '<br><b>Lattice vectors (Angstroms)</b>\n'
+    info_data += ('<br>{0:10.8f}&nbsp;{1:10.8f}&nbsp;{2:10.8f}\n'.format(*lattice_vectors[0]) +
+                  '<br>{0:10.8f}&nbsp;{1:10.8f}&nbsp;{2:10.8f}\n'.format(*lattice_vectors[1]) +
+                  '<br>{0:10.8f}&nbsp;{1:10.8f}&nbsp;{2:10.8f}\n'.format(*lattice_vectors[2]))
     info_data += '\n'
-    info_data += '<br>Positions (frac. coord)</br>\n'
+    info_data += '<br><b>Positions (frac. coord)</b>\n'
     for i, xyz in enumerate(positions):
-        info_data += ('{} '.format(species[i]) + '{0:10.8f} {1:10.8f} {2:10.8f}\n'.format(*xyz))
+        info_data += ('<br>{}&nbsp;'.format(species[i]) + '{0:10.8f}&nbsp;{1:10.8f}&nbsp;{2:10.8f}\n'.format(*xyz))
     info_data += '\n'
-    info_data += 'Volume {} Angstroms<sup>3</sup>\n'.format(volume)
+    info_data += '<br>Volume {} Angstroms<sup>3</sup>\n'.format(volume)
 
     return info_data
 
