df5e7867eb73667ac93b5b0127c58a65470d9cab
abel <abelcarreras83@gmail.com>
2017-09-19 16:21:34 +0900

    Testing vasp plugin

--- plugins/jobs/vasp/vasp.py
+++ plugins/jobs/vasp/vasp.py
@@ -29,7 +29,8 @@ def write_poscar(poscar, file='POSCAR'):
     poscar_txt += '\n1.0\n'
     cell = poscar_dict['structure']['lattice']['matrix']
     print poscar_txt
-
+    print  cell
+    exit()
     for row in cell:
         poscar_txt += '{0: 22.16f} {1: 22.16f} {2: 22.16f}\n'.format(*row)
     poscar_txt += ' '.join(np.unique([site['species']['element'] for site in poscar_dict['structure']['sites']])) + '\n'
