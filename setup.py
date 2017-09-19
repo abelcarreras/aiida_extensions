b9a7977a3157f60fe78564ba03711b7df798c133
abel <abelcarreras83@gmail.com>
2017-09-19 15:10:03 +0900

    Added setup.py installer and license

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aiida-phonon-phonopy',
+    version='0.1',
+    description='AiiDA plugin for running phonon calculations using phonopy',
+    url='https://github.com/abelcarreras/aiida_extensions',
+    author='Abel Carreras',
+    author_email='abelcarreras@gmail.com',
+    license='MIT license',
+    packages=find_packages(exclude=['aiida']),
+    entry_points={
+        'aiida.calculations': [
+            'lammps.combinate = plugins.jobs.lammps.combinate:CombinateCalculation',
+            'lammps.force = plugins.jobs.lammps.force:ForceCalculation',
+            'lammps.optimize = plugins.jobs.lammps.optimize:OptimizeCalculation'
+            'phonopy = plugins.jobs.phonopy: PhonopyCalculation'],
+            'dynaphopy = plugins.jobs.dynaphopy: DynaphopyCalculation'
+        'aiida.parsers': [
+            'lammps.force = plugins.parsers.lammps.force:ForceParser',
+            'lammps.optimize = plugins.parsers.lammps.optimize:OptimizeParser',
+            'phonopy = plugins.parsers.phonopy: PhonopyParser',
+            'dynaphopy = plugins.parsers.dynaphopy: DynaphopyParser'],
+        'aiida.workflows': [
+            'wf_phonon = workflows.wf_phonon : WorkflowPhonon',
+            'wf_grunesien_pressure = workflows.wf_gruneisen_pressure : WorkflowGruneisen',
+            'wf_grunesien_volume = workflows.wf_gruneisen_volume : WorkflowGruneisen',
+            'wf_qha = workflows.qha : WorkflowQHA',
+            'wf_quasiparticle = workflows.quasiparticle : WorkflowQuasiparticle',
+            'wf_quasiparticle_thermo = workflows.wf_quasiparticle_thermo : WorkflowQuasiparticle',
+            'wf_scan_quasiparticle = workflows.wf_scan_quasiparticle : WorkflowScanQuasiparticle',
+        ]
+    },
+    scripts=['launcher/launch_gruneisen_lammps_gan.py',
+             'launcher/launch_gruneisen_lammps_na.py',
+             'launcher/launch_gruneisen_lammps_si.py',
+             'launcher/launch_phonon_lammps_ar.py',
+             'launcher/launch_phonon_lammps_gan.py',
+             'launcher/launch_phonon_lammps_na.py',
+             'launcher/launch_quasiparticle_lammps_gan.py',
+             'launcher/launch_quasiparticle_scan.py',
+             'launcher/launch_quasiparticle_thermo_lammps.py']
+
+)
+
