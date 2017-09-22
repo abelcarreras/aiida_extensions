6d99648f6b4bf933186b48892800abb02020eb72
abel <abelcarreras83@gmail.com>
2017-09-22 15:55:38 +0900

    Updating to new plugin system

--- setup.py
+++ setup.py
@@ -17,12 +17,14 @@ setup(
             'lammps.force = plugins.jobs.lammps.force:ForceCalculation',
             'lammps.md = plugins.jobs.lammps.md:MdCalculation',
             'lammps.optimize = plugins.jobs.lammps.optimize:OptimizeCalculation',
+            # 'vasp.vasp = plugins.jobs.vasp:VaspCalculation',
             'phonopy = plugins.jobs.phonopy: PhonopyCalculation',
             'dynaphopy = plugins.jobs.dynaphopy: DynaphopyCalculation'],
         'aiida.parsers': [
             'lammps.force = plugins.parsers.lammps.force:ForceParser',
             'lammps.md = plugins.parsers.lammps.md:MdParser',
             'lammps.optimize = plugins.parsers.lammps.optimize:OptimizeParser',
+             # 'vasp.vasp = plugins.parsers.vasp:VaspParser',
             'phonopy = plugins.parsers.phonopy: PhonopyParser',
             'dynaphopy = plugins.parsers.dynaphopy: DynaphopyParser'],
         'aiida.workflows': [
