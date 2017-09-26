7b13e4ff26ce257ad9ffbb0f9aab5b59aebe7ee0
abel <abelcarreras83@gmail.com>
2017-09-26 16:51:32 +0900

    developing workchain

--- workchains/wc_phonon.py
+++ workchains/wc_phonon.py
@@ -678,8 +678,9 @@ if __name__ == "__main__":
                   machine=machine,
                   es_settings=es_settings,
                   ph_settings=ph_settings,
+                  # Optional settings
                   pressure=Float(10),
-                  optimize=Bool(False)
+                  optimize=Bool(0)
                   )
 
     # Check results
