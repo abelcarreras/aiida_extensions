e38834a60a5d33d13ce7eadd92f4bebe2da223f2
abel <abelcarreras83@gmail.com>
2017-09-27 12:13:41 +0900

    developing workchain

--- workchains/wc_phonon.py
+++ workchains/wc_phonon.py
@@ -731,7 +731,7 @@ if __name__ == "__main__":
 
     machine = ParameterData(dict=machine_dict)
 
-    results = run(FrozenPhonon2,
+    results = submit(FrozenPhonon2,
                   structure=structure,
                   machine=machine,
                   es_settings=es_settings,
