cce3fe537d041fdae87c5a60ce433e6de3fc30cf
abel <abelcarreras83@gmail.com>
2017-10-12 19:19:29 +0900

    Cleaning workchain phonon

--- workchains/wc_phonon.py
+++ workchains/wc_phonon.py
@@ -20,12 +20,11 @@ from aiida.orm.data.base import Str, Float, Bool
 #from aiida.orm.calculation.job.vasp.vasp import VaspCalculation
 from aiida.work.workchain import if_
 
-VaspCalculation = CalculationFactory('vasp.vasp')
 PwCalculation = CalculationFactory('quantumespresso.pw')
 PhonopyCalculation = CalculationFactory('phonopy')
 
 import numpy as np
-
+from generate_inputs import *
 
 # Function obtained from aiida's quantumespresso plugin. Copied here for convinence
 def get_pseudos(structure, family_name):
@@ -61,126 +60,6 @@ def get_pseudos(structure, family_name):
             pseudos[kind] = pseudo
 
     return pseudos
-
-def generate_qe_params(code, structure, machine, settings, kpoints, pseudo):
-    """
-    generate the input paramemeters needed to run a calculation for PW (Quantum Espresso)
-    :param code: aiida Code object
-    :param structure:  aiida StructureData object
-    :param machine: aiida ParametersData object containing a dictionary with the computational resources information
-    :param settings: aiida ParametersData object containing a dictionary with the input parameters for PW
-    :param kpoints: aiida KpointsData object
-    :param pseudo: aiida Str object containing the label of the pseudopotentals family to use
-    :return: Calculation process object, input dictionary
-    """
-
-
-    # The inputs
-    inputs = PwCalculation.process().get_inputs_template()
-    inputs.code = code
-
-    # The structure
-    inputs.structure = structure
-
-    # Machine
-    inputs._options.resources = machine.dict.resources
-    inputs._options.max_wallclock_seconds = machine.dict.max_wallclock_seconds
-
-    # Parameters
-    inputs.parameters = settings
-
-    # Kpoints
-    inputs.kpoints = kpoints
-
-    # Pseudopotentials
-    ######## MANUALTEST #########
-    manual_pseudo = False
-    if manual_pseudo:
-        # Pseudopotentials (test)
-        pseudo_dir = '/Users/abel/software/espresso/pbe/'
-        raw_pseudos = [
-           pseudo_dir + "Ga.pbe-dn-rrkjus_psl.1.0.0.UPF",
-           pseudo_dir + "N.pbe-n-rrkjus_psl.1.0.0.UPF"]
-
-        pseudos = {}
-
-        for file_path in raw_pseudos:
-            pseudo, created = UpfData.get_or_create(file_path, use_first=True)
-            pseudos.update({pseudo.element: pseudo})
-
-        inputs.pseudo = pseudos
-        return PwCalculation.process(), inputs
-    ######## MANUAL TEST #########
-
-    inputs.pseudo = get_pseudos(structure, pseudo)
-
-    return PwCalculation.process(), inputs
-
-
-def generate_vasp_params(structure, machine, settings):
-    """
-    generate the input paramemeters needed to run a calculation for VASP
-    :param structure:  aiida StructureData object
-    :param machine: aiida ParametersData object containing a dictionary with the computational resources information
-    :param settings: aiida ParametersData object containing a dictionary with the INCAR parameters
-    :return: Calculation process object, input dictionary
-    """
-
-    #plugin = self.inputs.es_settings.dict.code.get_attr('input_plugin')
-
-    # The inputs
-
-    inputs = VaspCalculation.process().get_inputs_template()
-
-    # code
-    inputs.code = Code.get_from_string(settings.dict.code)
-
-    # structure
-    inputs.structure = structure
-
-    inputs._options.resources = machine.dict.resources
-    inputs._options.max_wallclock_seconds = machine.dict.max_wallclock_seconds
-
-    # INCAR (parameters)
-    inputs.incar = ParameterData(dict=settings.dict.parameters)
-
-
-    # POTCAR (pseudo)
-    inputs.potcar = ParameterData(dict=settings.dict.pseudos)
-
-    settings_parse = {'PARSER_INSTRUCTIONS':[]}
-    pinstr = settings_parse['PARSER_INSTRUCTIONS']
-    pinstr += [{
-        'instr': 'array_data_parser',
-        'type': 'data',
-        'params': {}},
-        {
-        'instr': 'output_parameters',
-        'type': 'data',
-        'params': {}},
-        {
-        'instr': 'dummy_error_parser',
-        'type': 'error',
-        'params': {}},
-        {
-        'instr': 'default_structure_parser',
-        'type': 'structure',
-        'params': {}}
-    ]
-
-    # Kpoints
-    from pymatgen.io import vasp as vaspio
-
-    kpoints_pg = vaspio.Kpoints.automatic_density(structure.get_pymatgen_structure(), settings.dict.kpoints_per_atom)
-    #kpoints_pg = vaspio.Kpoints.monkhorst_automatic(kpts=[2, 2, 2],
-    #                                                shift=[0.0, 0.0, 0.0])
-
-    kpoints = ParameterData(dict=kpoints_pg.as_dict())
-    inputs.kpoints = kpoints
-
-    inputs.settings = ParameterData(dict=settings_parse)
-
-    return VaspCalculation.process(), inputs
 
 
 def generate_phonopy_params(code, structure, parameters, machine, data_sets):
