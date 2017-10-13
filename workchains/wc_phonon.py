ed02f109ddfb334dae56494088bde7b3880811f8
abel <abelcarreras83@gmail.com>
2017-10-13 14:32:21 +0900

    Add Oprimize workchain

--- workchains/wc_phonon.py
+++ workchains/wc_phonon.py
@@ -26,42 +26,6 @@ PhonopyCalculation = CalculationFactory('phonopy')
 import numpy as np
 from generate_inputs import *
 
-# Function obtained from aiida's quantumespresso plugin. Copied here for convinence
-def get_pseudos(structure, family_name):
-    """
-    Set the pseudo to use for all atomic kinds, picking pseudos from the
-    family with name family_name.
-
-    :note: The structure must already be set.
-
-    :param family_name: the name of the group containing the pseudos
-    """
-    from collections import defaultdict
-    from aiida.orm.data.upf import get_pseudos_from_structure
-
-    # A dict {kind_name: pseudo_object}
-    kind_pseudo_dict = get_pseudos_from_structure(structure, family_name)
-
-    # We have to group the species by pseudo, I use the pseudo PK
-    # pseudo_dict will just map PK->pseudo_object
-    pseudo_dict = {}
-    # Will contain a list of all species of the pseudo with given PK
-    pseudo_species = defaultdict(list)
-
-    for kindname, pseudo in kind_pseudo_dict.iteritems():
-        pseudo_dict[pseudo.pk] = pseudo
-        pseudo_species[pseudo.pk].append(kindname)
-
-    pseudos = {}
-    for pseudo_pk in pseudo_dict:
-        pseudo = pseudo_dict[pseudo_pk]
-        kinds = pseudo_species[pseudo_pk]
-        for kind in kinds:
-            pseudos[kind] = pseudo
-
-    return pseudos
-
-
 def generate_phonopy_params(code, structure, parameters, machine, data_sets):
     """
     Generate inputs parameters needed to do a remote phonopy calculation
