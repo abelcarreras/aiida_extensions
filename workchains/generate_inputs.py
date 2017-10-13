9b30c7a0d994bba585d605c13cd505198c80bd7a
abel <abelcarreras83@gmail.com>
2017-10-13 15:45:02 +0900

    Add Oprimize workchain

--- workchains/generate_inputs.py
+++ workchains/generate_inputs.py
@@ -107,7 +107,7 @@ def generate_qe_params(code, structure, machine, settings, kpoints, pseudo):
     return PwCalculation.process(), inputs
 
 
-def generate_lammps_params(structure, machine, settings, pressure=0.0, type=None):
+def generate_lammps_params(structure, machine, settings, plugin, pressure=0.0, type=None):
     """
     generate the input paramemeters needed to run a calculation for LAMMPS
     :param structure:  aiida StructureData object
@@ -117,16 +117,8 @@ def generate_lammps_params(structure, machine, settings, pressure=0.0, type=None
     """
 
     print 'start generate', type
-    if type is None:
-        code = settings.dict.code
 
-    else:
-        print 'get code', settings.dict.code
-        code = settings.dict.code[type]
-
-    print ('code', code)
-
-    LammpsCalculation = CalculationFactory(code)
+    LammpsCalculation = CalculationFactory(plugin)
 
     inputs = LammpsCalculation.process().get_inputs_template()
 
@@ -149,7 +141,7 @@ def generate_lammps_params(structure, machine, settings, pressure=0.0, type=None
     return LammpsCalculation.process(), inputs
 
 
-def generate_vasp_params(structure, machine, settings, type=None):
+def generate_vasp_params(structure, machine, settings, plugin, type=None):
     """
     generate the input paramemeters needed to run a calculation for VASP
     :param structure:  aiida StructureData object
@@ -160,7 +152,7 @@ def generate_vasp_params(structure, machine, settings, type=None):
 
     # plugin = self.inputs.es_settings.dict.code.get_attr('input_plugin')
 
-    VaspCalculation = CalculationFactory(settings.dict.code)
+    VaspCalculation = CalculationFactory(plugin)
 
     inputs = VaspCalculation.process().get_inputs_template()
 
@@ -227,14 +219,14 @@ def generate_inputs(structure, machine, es_settings, type=None):
 
     print 'plugin', plugin
     if plugin == 'vasp.vasp':
-        return generate_vasp_params(structure, machine, es_settings, type=type)
+        return generate_vasp_params(structure, machine, es_settings, plugin, type=type)
 
     # elif plugin == 'quantumespresso.pw':
     #     return generate_qe_params(structure, machine, es_settings)
 
     elif plugin in ['lammps.forces', 'lammps.optimize', 'lammps.md']:
         print 'yeah!'
-        return generate_lammps_params(structure, machine, es_settings, type=type)
+        return generate_lammps_params(structure, machine, es_settings, plugin, type=type)
     else:
         print 'No supported plugin'
         exit()
