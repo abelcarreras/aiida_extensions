1e38e0d5beec6411dc465e972c2fd20691ce1170
abel <abelcarreras83@gmail.com>
2017-10-18 16:47:27 +0900

    Cleaning wc_phonon workchain

--- workchains/launcher/launch_phonon.py
+++ workchains/launcher/launch_phonon.py
@@ -55,7 +55,7 @@ ph_settings = ParameterData(dict={'supercell': [[2,0,0],
                                   })
 
 # VASP SPECIFIC
-if True:   # Set TRUE to use VASP or FALSE to use Quantum Espresso
+if False:   # Set TRUE to use VASP or FALSE to use Quantum Espresso
     incar_dict = {
         'NELMIN' : 5,
         'NELM'   : 100,
@@ -118,7 +118,7 @@ if False:
 
 
 # LAMMPS SPECIFIC
-if False:
+if True:
     # GaN Tersoff potentials parameters (can be readed from a file if needed)
     tersoff_gan = {
         'Ga Ga Ga': '1.0 0.007874 1.846 1.918000 0.75000 -0.301300 1.0 1.0 1.44970 410.132 2.87 0.15 1.60916 535.199',
