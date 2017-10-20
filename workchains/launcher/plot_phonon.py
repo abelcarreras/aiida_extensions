ecca905fc0e28fe5e2a4f671c61c3f3df378d4cd
abel <abelcarreras83@gmail.com>
2017-10-20 16:54:22 +0900

    Added phonon dos object

--- workchains/launcher/plot_phonon.py
+++ workchains/launcher/plot_phonon.py
@@ -31,7 +31,7 @@ plt.xlim([0, bs.get_distances()[-1][-1]])
 plt.axhline(y=0, color='k', ls='dashed')
 plt.suptitle('Phonon band structure')
 
-print bs.get_formatted_labels_matplotlib
+print bs.get_formatted_labels_matplotlib()
 
 if bs.get_labels() is not None:
     plt.rcParams.update({'mathtext.default': 'regular' })
