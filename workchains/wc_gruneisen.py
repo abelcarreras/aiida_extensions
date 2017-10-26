d932ff0c34e7da9cc74c086d60955fa81b999260
abel <abelcarreras83@gmail.com>
2017-10-26 17:03:53 +0900

    Add gruneisen plot tool

--- workchains/wc_gruneisen.py
+++ workchains/wc_gruneisen.py
@@ -121,11 +121,11 @@ class GruneisenPhonopy(WorkChain):
         print 'start create cell expansions'
 
         # For testing
-        testing = False
+        testing = True
         if testing:
-            self.ctx._content['plus'] = load_node(4949)
-            self.ctx._content['origin'] = load_node(4946)
-            self.ctx._content['minus'] = load_node(4952)
+            self.ctx._content['plus'] = load_node(5858)
+            self.ctx._content['origin'] = load_node(5855)
+            self.ctx._content['minus'] = load_node(5861)
             return
 
         calcs = {}
