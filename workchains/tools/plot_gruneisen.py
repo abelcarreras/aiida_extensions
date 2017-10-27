91857cc2330046dc13a83f5b85d9fb4c283faaf9
abel <abelcarreras83@gmail.com>
2017-10-27 12:19:54 +0900

    Add gruneisen plot tool

--- workchains/tools/plot_gruneisen.py
+++ workchains/tools/plot_gruneisen.py
@@ -34,7 +34,7 @@ plt.xticks(label_positions, labels, rotation='horizontal')
 plt.xlabel('Wave vector')
 
 plt.figure(1)
-for dist, freq in zip(bs.get_distances(), bs.get_frequencies()):
+for dist, freq in zip(bs.get_my_distances(), bs.get_frequencies()):
     plt.plot(dist, freq, color='r')
 plt.ylabel('Frequency [THz]')
 plt.title('Phonon band structure')
