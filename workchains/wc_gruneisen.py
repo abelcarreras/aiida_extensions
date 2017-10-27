b2951915bb89f59d9bda8cdea7e717490bff001b
abel <abelcarreras83@gmail.com>
2017-10-27 10:40:29 +0900

    Add gruneisen plot tool

--- workchains/wc_gruneisen.py
+++ workchains/wc_gruneisen.py
@@ -105,11 +105,11 @@ def phonopy_gruneisen(phonon_plus_structure,
     #gruneisen.set_band_structure(band_structure.get_band_ranges(),
     #                             band_structure.get_number_of_points())
     #gruneisen.set_band_structure(band_structure.get_bands(), 31)
+    #band_structure.set_band_structure_gruneisen(gruneisen.get_band_structure())
 
     bands = get_path_using_seekpath(phonon_origin_structure)
     gruneisen.set_band_structure(bands['ranges'], 31)
 
-    band_structure.set_band_structure_gruneisen(gruneisen.get_band_structure())
 
 
     # mesh
