e36cba648ec09356c1d104745c411593453d26be
abel <abelcarreras83@gmail.com>
2017-10-27 12:18:19 +0900

    Add gruneisen plot tool

--- data/band_structure.py
+++ data/band_structure.py
@@ -131,7 +131,10 @@ class BandStructureData(Data):
 
         distances = []
         for band in array:
-            band_dist = [0.0]
+            if distances == []:
+                band_dist = [0.0]
+            else:
+                band_dist = distances[-1][-1]
             for i in range(npoints-1):
                 band_dist.append(np.linalg.norm(np.array(band[i+1]) - np.array(band[i]))+band_dist[i])
 
