c1be7e178b6ba35f4099bfc64367ff714b915cb5
abel <abelcarreras83@gmail.com>
2017-10-30 15:31:36 +0900

    Add gruneisen plot tool

--- workchains/tools/plot_gruneisen.py
+++ workchains/tools/plot_gruneisen.py
@@ -79,9 +79,9 @@ for i, freq in enumerate(mesh.get_array('frequencies').T):
     gamma = mesh.get_array('gruneisen').T[i]
     q_points = mesh.get_array('q_points').T[i]
     print q_points
-
-    mask = np.where(np.linalg.norm(q_points, axis=1) > gamma_cutoff)
-    print mask
+    print '--'
+    #mask = np.where(np.linalg.norm(q_points, axis=1) > gamma_cutoff)
+    #print mask
     plt.plot(freq, gamma, marker='o', linestyle='None', markeredgecolor='black', color='red')
 plt.xlabel('Frequency [THz]')
 plt.ylabel('$\gamma$')
