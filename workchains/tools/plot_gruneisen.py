8d9c8f03ecf168c767e023a0ea243f3fe21eaae7
abel <abelcarreras83@gmail.com>
2017-10-30 15:30:23 +0900

    Add gruneisen plot tool

--- workchains/tools/plot_gruneisen.py
+++ workchains/tools/plot_gruneisen.py
@@ -77,7 +77,7 @@ plt.figure(4)
 
 for i, freq in enumerate(mesh.get_array('frequencies').T):
     gamma = mesh.get_array('gruneisen').T[i]
-    q_points = mesh.get_array('q_points')[i]
+    q_points = mesh.get_array('q_points').T[i]
     print q_points
 
     mask = np.where(np.linalg.norm(q_points, axis=1) > gamma_cutoff)
