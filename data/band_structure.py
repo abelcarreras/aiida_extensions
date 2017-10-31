219a09202d0d586158c85b548592ecbfba10fe88
abel <abelcarreras83@gmail.com>
2017-10-31 17:16:42 +0900

    Improving plot phonon data

--- data/band_structure.py
+++ data/band_structure.py
@@ -301,10 +301,10 @@ class BandStructureData(Data):
             if labels_array[i+1][0] == labels_array[i][1]:
                 block.append(replace_list(labels_array[i+1][0], substitutions))
             else:
-                block.append(replace_list(labels_array[i+1][1], substitutions))
+                block.append(replace_list(labels_array[i][1], substitutions))
                 labels.append(block)
                 print block
-                block = [replace_list(labels_array[i-1][0], substitutions)]
+                block = [replace_list(labels_array[i+1][0], substitutions)]
 
         print labels
 
