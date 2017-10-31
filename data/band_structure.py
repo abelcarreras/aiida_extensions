676dfd7580076f932a6577b79a037bac546d5a43
abel <abelcarreras83@gmail.com>
2017-10-31 18:12:36 +0900

    Improving plot phonon data

--- data/band_structure.py
+++ data/band_structure.py
@@ -305,7 +305,6 @@ class BandStructureData(Data):
                 block_positions.append(i+1)
             else:
                 block.append(replace_list(labels_array[i][1], substitutions))
-                block_positions.append(i)
                 labels.append(block)
                 labels_positions.append(block_positions)
 
@@ -314,10 +313,8 @@ class BandStructureData(Data):
 
                 #print block
         block.append(replace_list(labels_array[-1][1], substitutions))
-        block_positions.append(len(distances))
 
         labels.append(block)
-        labels_positions.append(block_positions)
 
         print labels_positions
         exit()
