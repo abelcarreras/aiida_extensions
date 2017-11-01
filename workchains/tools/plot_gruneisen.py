779271453663f3412ab0d74d9c5445877d8cad8c
abel <abelcarreras83@gmail.com>
2017-11-01 15:31:33 +0900

    Improving plot phonon data

--- workchains/tools/plot_gruneisen.py
+++ workchains/tools/plot_gruneisen.py
@@ -77,8 +77,8 @@ for j, index in enumerate(indices):
 
     if j != 0:
         ax1.axes.get_yaxis().set_visible(False)
-        ax0 = plt.subplot(gs[j-1])
-        ax1.set_ylim(ax0.get_ylim())
+        #ax0 = plt.subplot(gs[j-1])
+        #ax1.set_ylim(ax0.get_ylim())
 
     plt.axhline(y=0.0, color='black', linestyle='--', linewidth=0.1)
     plt.ylabel('$\gamma$')
