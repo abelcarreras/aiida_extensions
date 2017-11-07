72184a2efeb2d127d1904e7f8c481aa3bfa1ba8c
abel <abelcarreras83@gmail.com>
2017-11-07 15:09:35 +0900

    Adding QE support

--- workchains/wc_optimize.py
+++ workchains/wc_optimize.py
@@ -67,8 +67,8 @@ class OptimizeStructure(WorkChain):
         spec.input("pressure", valid_type=Float, required=False, default=Float(0.0))
         spec.input("tolerance_forces", valid_type=Float, required=False, default=Float(1e-5))
         spec.input("tolerance_stress", valid_type=Float, required=False, default=Float(1e-2))
-        spec.input("standarize_cell", valid_type=Bool, required=False, default=Bool(0))
         spec.input("max_iterations", valid_type=Int, required=False, default=Int(3))
+        spec.input("standarize_cell", valid_type=Int, required=False, default=Int(0))
 
         spec.outline(cls.optimize_cycle, _While(cls.not_converged)(cls.optimize_cycle), cls.get_data)
 
