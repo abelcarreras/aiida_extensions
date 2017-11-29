0d497bab6003a2fa3bf0e75c23fcc902a3998c0c
abel <abelcarreras83@gmail.com>
2017-11-29 11:06:32 +0900

    Improving lammps-phonopy plugin to use new dynaphopy functionality

--- plugins/launcher/launch_lammps_md_si.py
+++ plugins/launcher/launch_lammps_md_si.py
@@ -52,7 +52,7 @@ lammps_machine = {
 
 
 parameters_md = {'timestep': 0.001,
-                 'temperature' : 700,
+                 'temperature' : 900,
                  'thermostat_variable': 0.5,
                  'equilibrium_steps': 1000000,
                  'total_steps': 200000,
